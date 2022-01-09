a791c75756fa6b436d06390b9e30a8936fa0dec1
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-01-09 18:27:29 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function flatten(arr) {
+    return arr.reduce(function (flat, toFlatten) {
+        return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+    }, []);
+}
+
+console.log(flatten([[1, 2, 3], [4, 5]])); 
+console.log(flatten([[[1, [1.1]], 2, 3], [4, 5]])); 
+
+
