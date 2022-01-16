9be556e52fedcee89e7c30f8809f1c37b4c68d37
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-01-16 08:47:54 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+"use strict";
+(function () {
 
+const form = document.querySelector('#form');
+//let inputData = form.querySelector('input');
+
+
+form
+    .addEventListener('submit', event => {
+            event.preventDefault();
+
+            let inpData = Array.from(
+                event.target.querySelectorAll('input, textarea')
+            )
+                .reduce((accum,item) => {
+                    accum[item.name] = item.value;
+                    return accum;
+                }, {})
+                console.log(inpData);
+                event.target.reset();
+            
+        })
+
+
+
+
+
+
+
+
+
+})();
 
 
