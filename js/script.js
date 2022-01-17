1ce0bd6b7be9e6aa1c850e1d2de107360cd8d65b
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2022-01-17 12:42:27 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 (function () {
 
 const form = document.querySelector('#form');
-//let inputData = form.querySelector('input');
+
+let inputData = form.querySelector('.bgcolor');
+
+// if (inputData.value ==='') {
+//     inputData.style.backgroundColor = 'red';
+// } else {
+//     inputData.style.backgroundColor = 'white';
+// }
+inputData.onblur = function() {
+    if (inputData.value ==='') {
+        this.classList.add('bgcolor')
+    }  inputData.classList.remove('bgcolor');
+}
 
 
 form
     .addEventListener('submit', event => {
             event.preventDefault();
-
+            
             let inpData = Array.from(
                 event.target.querySelectorAll('input, textarea')
             )
@@ -16,19 +28,19 @@ form
                     accum[item.name] = item.value;
                     return accum;
                 }, {})
-
+            
                 console.log(inpData);
                 event.target.reset();
                 createElement(inpData);
             
         })
-
+        
 function createElement(elementData) {
-   const div = document.createElement('div');
-   div.classList.add('data');
-   div.innerHTML = JSON.stringify(elementData);
-   
-   document.querySelector('.vueData').append(div);
+    const div = document.createElement('div');
+    div.classList.add('data');
+    div.innerHTML = JSON.stringify(elementData);
+
+    document.querySelector('.vueData').append(div);
 }
 
 
