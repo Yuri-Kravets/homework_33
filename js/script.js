485c87862bf33a39af248d2ba7646957804c2993
Yuri-Kravets <kravets.yuri.v@gmail.com>
2022-01-28 11:04:28 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,51 +40,8 @@ function createElement(elementData) {
     let div = document.createElement('div');
     div.classList.add('data');
     div.innerHTML = JSON.stringify(elementData);
-
-    document.querySelector('.vueData').append(div);
-    
+    document.querySelector('.vueData').append(div);   
 }
-
-
-
-
-
-
-
-
-// inputData.onblur = function() {
-//     if (inputData.value ==='') {
-//         this.classList.add('bgcolor')
-//     }  inputData.classList.remove('bgcolor');
-// }
-
-// function validateForm (form) {
-//     let error = 0;
-//     let formReq = document.querySelectorAll('_req');
-
-//     for (let i = 0;i < formReq.length;i++) {
-//         const input = formReq[i];
-//         formRemoveError(input);
-//         if (input.classList.contains('_req')) {
-//             formAddError(input);
-//             error++;
-//         }
-//     }
-// }
-// function formAddError(input) {
-//     input.parentElement.classList.add('_error');
-//     input.classList.add('_error');
-// }
-
-// function formRemoveError(input) {
-//     input.parentElement.classList.remove('_error');
-//     input.classList.remove('_error');
-//}
-
-
-
-
-
 
 })();
 
